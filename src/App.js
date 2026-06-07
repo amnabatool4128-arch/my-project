bd6c282dd79369d8c10132ccc4993f0de06f27f2
Amna Batool <amnabatool4128@gmail.com>
2026-06-07 06:14:11 +0500

    Add Tailwind CSS integration and refactor App component
    - Introduced Tailwind CSS as a dev dependency.
    - Removed App.css and updated App.js to utilize Tailwind for styling.
    - Cleaned up index.css to only include Tailwind directives.
    - Created tailwind.config.js for Tailwind configuration.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [color, setColor] = useState("#60a5eb");
+  const colors = [
+    "#ef4444",
+    "#22c55e",
+    "#3b82f6",
+    "#8b5cf6",
+    "#f97316",
+    "#ec4899",
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="w-full h-screen transition-all duration-500"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center top-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-4 shadow-lg bg-white px-3 py-2 rounded-2xl">
+          {
+  colors.map((item) => (
+    <button
+      key={item}
+      onClick={() => setColor(item)}
+      style={{ backgroundColor: item }}
+      className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+    >
+      {item}
+    </button>
+  ))}
+  
+
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
+
+
+
